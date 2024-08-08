23bc3a251200e44eee68aa3383d20ea5be7be8b2
Amir-1234 <78350042+Amir-1234@users.noreply.github.com>
2024-08-08 14:08:28 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '97cf2475-6007-4f0b-92ee-a81ee087d234';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
